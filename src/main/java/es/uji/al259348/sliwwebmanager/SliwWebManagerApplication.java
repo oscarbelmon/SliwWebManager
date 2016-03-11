52ec4b27a7860c2823e2b739f2bc41a3541945f3
Adrian Puertas Cabedo <al259348@uji.es>
2016-03-11 10:29:01 +0100

    Creación de la SpringBootApplication

---
+++ src/main/java/es/uji/al259348/sliwwebmanager/SliwWebManagerApplication.java
@@ -0,0 +1,13 @@
+package es.uji.al259348.sliwwebmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliwWebManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliwWebManagerApplication.class, args);
+	}
+
+}
