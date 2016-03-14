fdc4e2cf03dc62a891489fec87c48f52cb7cbbbb
Adrian Puertas Cabedo <al259348@uji.es>
2016-03-14 13:56:05 +0100

    UserService método save

--- src/main/java/es/uji/al259348/sliwwebmanager/services/UserService.java
+++ src/main/java/es/uji/al259348/sliwwebmanager/services/UserService.java
@@ -2,10 +2,9 @@ package es.uji.al259348.sliwwebmanager.services;
 
 import es.uji.al259348.sliwwebmanager.model.User;
 
-import java.util.List;
-
 public interface UserService {
 
     Iterable<User> findAll();
+    User save(User user);
 
 }
