106b24051073810f5e425b800cbd418195ddcdf8
Adrian Puertas Cabedo <al259348@uji.es>
2016-03-18 10:20:11 +0100

    Seleccionar tamaño del listado de dispositivos

--- src/main/java/es/uji/al259348/sliwwebmanager/controllers/DevicesController.java
+++ src/main/java/es/uji/al259348/sliwwebmanager/controllers/DevicesController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.UUID;
 
 @Controller
 @RequestMapping(path = "devices")
