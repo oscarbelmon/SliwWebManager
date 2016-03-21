c30528708bad70dcc2c59421b1e52e3586fd722e
Adrian Puertas Cabedo <al259348@uji.es>
2016-03-21 13:29:37 +0100

    Añadido campo Mac a DeviceForm + validación Mac

--- src/main/java/es/uji/al259348/sliwwebmanager/model/forms/DeviceForm.java
+++ src/main/java/es/uji/al259348/sliwwebmanager/model/forms/DeviceForm.java
@@ -2,15 +2,23 @@ package es.uji.al259348.sliwwebmanager.model.forms;
 
 import es.uji.al259348.sliwwebmanager.model.Device;
 import es.uji.al259348.sliwwebmanager.model.validation.annotations.DeviceIdAvailable;
+import es.uji.al259348.sliwwebmanager.model.validation.annotations.DeviceMacAvailable;
 import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Pattern;
 
 public class DeviceForm {
 
-    @NotBlank
+    @NotBlank(message = "No puede estar vacío.")
     @DeviceIdAvailable
     private String id;
 
-    @NotBlank
+    @NotBlank(message = "No puede estar vacío.")
+    @Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", message = "No es una dirección MAC válida.")
+    @DeviceMacAvailable
+    private String mac;
+
+    @NotBlank(message = "No puede estar vacío.")
     private String name;
 
     public DeviceForm() {
@@ -25,6 +33,14 @@ public class DeviceForm {
         this.id = id;
     }
 
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
     public String getName() {
         return name;
     }
@@ -37,6 +53,8 @@ public class DeviceForm {
     public String toString() {
         return "DeviceForm{" +
                 "id='" + id + '\'' +
+                ", mac='" + mac + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
 
@@ -44,6 +62,7 @@ public class DeviceForm {
         Device device = new Device();
 
         device.setId(this.getId());
+        device.setMac(this.getMac());
         device.setName(this.getName());
 
         return device;
