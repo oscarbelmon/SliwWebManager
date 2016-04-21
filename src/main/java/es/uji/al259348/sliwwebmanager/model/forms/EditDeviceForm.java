89adc5842d922aac2b1b257ef52babf50fd73903
Adrian Puertas Cabedo <al259348@uji.es>
2016-04-21 11:45:31 +0200

    Formulario para editar dispositivo

---
+++ src/main/java/es/uji/al259348/sliwwebmanager/model/forms/EditDeviceForm.java
@@ -0,0 +1,30 @@
+package es.uji.al259348.sliwwebmanager.model.forms;
+
+import es.uji.al259348.sliwwebmanager.model.Device;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class EditDeviceForm {
+
+    @NotBlank(message = "No puede estar vacío.")
+    private String name;
+
+    public EditDeviceForm() {
+
+    }
+
+    public EditDeviceForm(Device device) {
+        this.name = device.getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void update(Device device) {
+        device.setName(name);
+    }
+}
