bda548a53b5d0751c836bed527203d6ce611a955
Adrian Puertas Cabedo <al259348@uji.es>
2016-06-06 22:53:57 +0200

    Enable elasticsearch repositories to be found... (only happens on laptop ¿?)

--- src/main/java/es/uji/al259348/sliwwebmanager/SliwWebManagerApplication.java
+++ src/main/java/es/uji/al259348/sliwwebmanager/SliwWebManagerApplication.java
@@ -2,8 +2,10 @@ package es.uji.al259348.sliwwebmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "es.uji.al259348.sliwwebmanager.repositories.elasticsearch", considerNestedRepositories = true)
 public class SliwWebManagerApplication {
 
 	public static void main(String[] args) {
